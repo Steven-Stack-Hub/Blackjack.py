220929978a305bd7b9460227d6771086a9d30389
Steven Sardar <steven1sardar@gmail.com>
2025-08-27 00:37:35 -0400

    recommited BJ Changes

---
+++ BJ Code.py
@@ -0,0 +1,33 @@
+import random
+from re import A
+
+cards = []
+suits = ["hearts", "diamonds", "clubs", "spades"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+for suit in suits:
+   for rank in ranks:
+        cards.append([suit, rank])
+
+
+def shuffle():
+    random.shuffle(cards)
+   
+def deal(number):
+    cards_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+shuffle()
+cards_dealt  = deal(2)
+card = cards_dealt[0]
+rank = card[1]
+
+if rank == "A":
+    value = 11
+elif rank == "J" or rank == "Q" or rank == "K":
+    value = 10
+else:
+    value = int(rank)
+printrank_dict = {'rank': rank, 'value': value}
